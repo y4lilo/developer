4441e6581f11bda7c6fb0c1e61876df84bfe36b4
Yael Linares <yaelprivado@gmail.com>
2024-08-30 19:31:31 +0200

    actualizacion

--- python/contadorPrimos/contadorPrimos.py
+++ python/contadorPrimos/contadorPrimos.py
@@ -1,5 +1,5 @@
 """Escribe una función llamada contar_primos() que requiera un solo argumento numérico.
-Esta función va a mostrar en pantalla todos los números primos existentes en el rango que va desde cero hasta ese
+Esta función va a mostrar en pantalla todos los números primos existentes en el rango que va desde dos hasta ese
 número incluido, y va a devolver la cantidad de números primos que encontró."""
 
 
